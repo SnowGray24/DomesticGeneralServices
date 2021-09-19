927428f6d7ce1ecae8a7dbfc6d78610f8032a76b
Cláudio Alves <aruakise2016@gmail.com>
2021-09-18 21:41:17 -0300

    Continuação da implementação da tela de Cadastro do Prestador

--- src/CadastroPrestador.js
+++ src/CadastroPrestador.js
@@ -1,43 +1,67 @@
 import './css/layout.css';
+import './css/button.css';
 import logo from './img/logo2.PNG';
 
 import BarProgress from './components/BarProgress';
 
-import React from 'react';
-import { Row, Form, FormGroup, Label, Input } from 'reactstrap';
-
+import React, {useState} from 'react';
+import { Col, Row, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
 function CadastroPrestador (){
+    const [fieldForm, setFieldForm] = useState(1);
+
+    const proxForm = () =>{
+        if (fieldForm === 1){
+            document.getElementById("formField1").style.display = "none";
+            document.getElementById("formField2").style.display = "block";
+        }else if (fieldForm === 2){
+            document.getElementById("formField1").style.display = "none";
+            document.getElementById("formField2").style.display = "none";
+        }
+
+        setFieldForm(fieldForm + 1);
+        verificationLimits();
+
+    }
+
+    const verificationLimits = () =>{
+        if (fieldForm > 3){
+            setFieldForm(4);
+        }else if (fieldForm < 1){
+            setFieldForm(1);
+        }
+    }
+
 
     return (
         <div className="cadastroPrestador">
             <section className="layoutBranco layoutCentralizar">
        
-                <img height='85' src={logo} alt="Logo da marca Torxs Services." />
+                <img className="logoSite" height='85' src={logo} alt="Logo da marca Torxs Services." />
                 
-                <span className="fontConfig">Conheça seus serviços com qualidade e</span>
+                <span style={{marginTop: "1em"}} className="fontConfig">Conheça seus serviços com qualidade e</span>
                 <span className="fontConfig">resposabilidade. Cadastre-se no Torxs Services!</span>
 
-                <nav className="layoutOptions">
+                <nav style={{marginTop: "30px"}} className="layoutOptions">
                     <Row>
-                    <BarProgress numero="1" opcao="Dados Pessoais" />
-                    <BarProgress numero="2" opcao="Endereço" />
-                    <BarProgress numero="3" opcao="Serviços" />
-                    <BarProgress numero="4" opcao="Dados do Login" />
+                        <BarProgress numero={1} opcao="Dados Pessoais" status={fieldForm} />
+                        <BarProgress numero={2} opcao="Endereço" status={fieldForm} />
+                        <BarProgress numero={3} opcao="Serviços" status={fieldForm} />
+                        <BarProgress numero={4} opcao="Dados do Login" status={fieldForm} />
                     </Row>
                 </nav>
 
 
-                <Form>
+                <Form id="formField1" style={{marginTop: "37px"}} className="formConfigCadastroPrestador">
 
                     <FormGroup>
-                        <Label for="exampleEmail">Nome</Label>
+                        <Label for="exampleEmail"><span className="campoObrigatorio">*</span> Nome</Label>
                         <Input type="email" name="email" id="exampleEmail" />
                     </FormGroup>
 
                     <FormGroup>
-                        <Label for="exampleCPF">CPF</Label>
+                        <Label for="exampleCPF"><span className="campoObrigatorio">*</span> CPF</Label>
                         <Input type="text" name="cpf" id="exampleCPF" />
                     </FormGroup>
 
@@ -46,22 +70,31 @@ function CadastroPrestador (){
                         <Input type="text" name="cnpj" id="exampleCNPJ" />
                     </FormGroup>
 
-                    <FormGroup>
-                        <Label for="exampleTelefone">Telefone</Label>
-                        <Input type="text" name="telefone" id="exampleTelefone" />
-                    </FormGroup>
+
+                    <Row>
+                        <Col>
+                            <FormGroup>
+                                <Label for="exampleTelefone"><span className="campoObrigatorio">*</span> Telefone</Label>
+                                <Input type="text" name="telefone" id="exampleTelefone" />
+                            </FormGroup>
+                        </Col>
+
+                        <Col>
+                            <FormGroup>
+                                <Label for="exampleDate"><span className="campoObrigatorio">*</span> Data de Nascimento</Label>
+                                <Input
+                                type="date"
+                                name="date"
+                                id="exampleDate" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+
 
                     <FormGroup>
-                        <Label for="exampleDate">Data</Label>
-                        <Input
-                        type="date"
-                        name="date"
-                        id="exampleDate" />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label>Sexo</Label>
-                        <Input type="select" bsSize="sm">
+                        <Label><span className="campoObrigatorio">*</span> Sexo</Label>
+                        <Input type="select" bsSize="sm" placeholder="Escolha um gênero">
                             <option>Masculino</option>
                             <option>Feminino</option>
                             <option>Outros</option>
@@ -69,6 +102,59 @@ function CadastroPrestador (){
                     </FormGroup>
 
                 </Form>
+
+
+
+                <Form id="formField2" style={{marginTop: "37px", display: "none"}} className="formConfigCadastroPrestador">
+                    <FormGroup>
+                        <Label><span className="campoObrigatorio">*</span> UF</Label>
+                        <Input type="select" bsSize="sm" placeholder="Escolha um gênero">
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                        </Input>
+                    </FormGroup>
+
+
+                    <FormGroup>
+                        <Label for="exampleCEP"><span className="campoObrigatorio">*</span> CEP</Label>
+                        <Input type="text" name="cep" id="exampleCEP" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleCidade"><span className="campoObrigatorio">*</span> Cidade</Label>
+                        <Input type="text" name="cidade" id="exampleCidade" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleRua"><span className="campoObrigatorio">*</span> Rua</Label>
+                        <Input type="text" name="rua" id="exampleRua" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleBairro"><span className="campoObrigatorio">*</span> Bairro</Label>
+                        <Input type="text" name="bairro" id="exampleBairro" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleNumero"><span className="campoObrigatorio">*</span> Número</Label>
+                        <Input type="text" name="numero" id="exampleNumero" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="examplePReferencia">Ponto de Referência</Label>
+                        <Input type="text" name="pontoDEreferencia" id="examplePReferencia" />
+                    </FormGroup>
+                </Form>
+
+
+                <div style={{marginTop: "30px"}}>
+                    <button className="btCancelar">Cancelar</button>
+                    <button className="btProx" onClick={proxForm} >Continuar</button>
+                </div>
+
+                <p style={{marginTop: "14px"}}>Já tem uma conta? <a href="/" className="facaLogin">Faça login.</a></p>
+
 
 
             </section>
--- src/components/BarProgress.js
+++ src/components/BarProgress.js
@@ -4,12 +4,31 @@ import React from 'react';
 import {Col } from 'reactstrap';
 
 function BarProgress (props){
+    let barProgress;
+    let numberProgress;
+    let optionProgress;
+    let status;
 
+    if(props.numero === props.status){
+        status = 1;
+    }else{
+        status = 0;
+    }
+    
+    if (status === 0){
+        barProgress = "barProgress";
+        numberProgress = "numberProgress";
+        optionProgress = "optionProgress";
+    }else{
+        barProgress = "barProgressActive";
+        numberProgress = "numberProgressActive";
+        optionProgress = "optionProgressActive"; 
+    }
     return (
         <Col>
-            <div className="barProgress">
-                        <div className="numberProgress">{props.numero}</div>
-                        <div className="optionProgress">{props.opcao}</div>
+            <div className={barProgress}>
+                    <div className={numberProgress}>{props.numero}</div>
+                    <div className={optionProgress}>{props.opcao}</div>
             </div>
         </Col>
     );
