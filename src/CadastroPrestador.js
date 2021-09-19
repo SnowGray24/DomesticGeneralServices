2c4c15e98760d5c5a5c5bd3856df1bf360a961f4
IsmaelRDeMelo <ismaeldemelo237@hotmail.com>
2021-09-19 16:55:53 -0300

    Adicionando todas as 27 UF

--- src/CadastroPrestador.js
+++ src/CadastroPrestador.js
@@ -109,9 +109,34 @@ function CadastroPrestador (){
                     <FormGroup>
                         <Label><span className="campoObrigatorio">*</span> UF</Label>
                         <Input type="select" bsSize="sm" placeholder="Escolha um gênero">
-                            <option></option>
-                            <option></option>
-                            <option></option>
+                            <option>AC</option>
+                            <option>AL</option>
+                            <option>AP</option>
+                            <option>AM</option>
+                            <option>BA</option>
+                            <option>CE</option>
+                            <option>DF</option>
+                            <option>ES</option>
+                            <option>GO</option>
+                            <option>MA</option>
+                            <option>MT</option>
+                            <option>MS</option>
+                            <option>MG</option>
+                            <option>PA</option>
+                            <option>PB</option>
+                            <option>PR</option>
+                            <option>PE</option>
+                            <option>PI</option>
+                            <option>BA</option>
+                            <option>RJ</option>
+                            <option>RN</option>
+                            <option>RS</option>
+                            <option>RO</option>
+                            <option>RR</option>
+                            <option>SC</option>
+                            <option>SP</option>
+                            <option>SE</option>
+                            <option>TO</option>
                         </Input>
                     </FormGroup>
 
