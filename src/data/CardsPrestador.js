f0c5311d215f8628d31f04c67c55c7e446aab8e8
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-19 20:58:09 -0300

    Criado arquivo com as informações dos cards do serviços solicitados ao prestador

---
+++ src/data/CardsPrestador.js
@@ -0,0 +1,14 @@
+const myServices = [
+    {
+        title: "Serviço de Mecânica",
+        prediction: "Previsão de término em 3 dias.",
+        isFinished: false,
+    },
+        {
+        title: "Serviço de Limpeza",
+        prediction: "Serviço finalizado.",
+        isFinished: true,
+    }
+]
+
+export default myServices;
