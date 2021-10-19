4675d1f653d61ab8cde7fe12443b1630b3b954ca
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-19 20:49:02 -0300

    Corrigidos erros referentes ao retorno de verificationItens e da key do map nos cards

--- src/components/MeusServicos.js
+++ src/components/MeusServicos.js
@@ -36,82 +36,74 @@ function MeusServicos(){
 
 
     
+    let card;
     const verificationItens = () =>{
-        if (listaServico.length === 0){
-            return (
-                <>
-                    <h5 style={{fontWeight: 'bold'}}>Nenhuma solicitação de serviços por aqui!</h5>
-                    <h6>Estamos de olho por aqui, assim que uma nova solicitação chegar, você será avisado.</h6>
-                </>
-            )
+         if (listaServico.length === 0){
+            card=<>
+                <h5 style={{fontWeight: 'bold'}}>Nenhuma solicitação de serviços por aqui!</h5>
+                <h6>Estamos de olho por aqui, assim que uma nova solicitação chegar, você será avisado.</h6>
+              </>
         }else{
-            return (
-                <div style={{position: 'absolute',width: '80%', height: '520px', overflow: 'scroll', scrollbarWidth: 'none'}}>
-                
-                <h5 style={{color: '#1351B4', marginTop: '28px'}}>
-                    Solicitações
-                </h5>
+            card= <div style={{position: 'absolute',width: '80%', height: '520px', overflow: 'scroll', scrollbarWidth: 'none'}}>
+                    <h5 style={{color: '#1351B4', marginTop: '28px'}}>
+                        Solicitações
+                    </h5>
 
-                <div style={{display: 'flex', marginTop: '2em'}}>
-                    {listaServico.map(obj =>(
-                        <Itens typeCard="solicitacao" chave={obj.Id} previcao={obj.Dias} titulo={obj.Titulo} image={obj.Img}  />
-                    ))}
+                    <div style={{display: 'flex', marginTop: '2em'}}>
+                        {listaServico.map((obj, index) =>(
+                            <Itens key={index} typeCard="solicitacao" chave={obj.Id} previcao={obj.Dias} titulo={obj.Titulo} image={obj.Img}  />
+                        ))}
+                    </div>
+
+                    <h5 style={{color: '#135184', marginTop: '18px'}}>
+                        Em andamento
+                    </h5>
+
+                    <div style={{display: 'flex', marginTop: '2em'}}>
+                        {listaServico.map((obj,index) =>(
+                            <Itens key={index} typeCard="emAndamento" chave={obj.Id} previcao={obj.Dias} titulo={obj.Titulo} image={imgIconFinalizado}  />
+                        ))}
+                    </div>
                 </div>
-
-
-                <h5 style={{color: '#135184', marginTop: '18px'}}>
-                    Em andamento
-                </h5>
-                <div style={{display: 'flex', marginTop: '2em'}}>
-                    {listaServico.map(obj =>(
-                        <Itens typeCard="emAndamento" chave={obj.Id} previcao={obj.Dias} titulo={obj.Titulo} image={imgIconFinalizado}  />
-                    ))}
-                </div>
-
-
-                </div>
-            )
-            
-           
         }
-    }
 
+    }
+    
+    verificationItens()
 
     return (
         <div>
+            { /* Title-bar */ }
+            <div style={{marginLeft: '30px',height: '60px', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <div>
+                    <img height="20" src={imgTask} alt="" /> {'>'} Meus Serviços
+                </div>
+            </div>
 
-                    { /* Title-bar */ }
-                    <div style={{marginLeft: '30px',height: '60px', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-                       <div>
-                           <img height="20" src={imgTask} alt="" /> {'>'} Meus Serviços
-                        </div>
+
+            { /* Options */ }
+            <div>
+                <nav className='barOptionsMeusServicos' style={{display: 'flex'}}>
+                    <div style={{display: 'flex'}}>
+                        <ul className='resetUlLi'  style={{marginLeft: '16px'}}>
+                            <li className="optionItemSelect" >Em andamento</li>
+                        </ul>
+
+                        <ul className='resetUlLi'>
+                            <li>Finalizado</li>
+                        </ul>
                     </div>
+                </nav>
 
+                <div style={{height: 'calc(100vh - 156px)', width: '100%'}} className='itemCentralizar'>
+                    
+                    {card}
 
-                    { /* Options */ }
-                    <div>
-                        <nav className='barOptionsMeusServicos' style={{display: 'flex'}}>
-                           
-                           <div style={{display: 'flex'}}>
-                                <ul className='resetUlLi'  style={{marginLeft: '16px'}}>
-                                    <li className="optionItemSelect" >Em andamento</li>
-                                </ul>
+                </div>
 
-                                <ul className='resetUlLi'>
-                                    <li >Finalizado</li>
-                                </ul>
-                            </div>
-                        </nav>
-
-                        <div style={{height: 'calc(100vh - 156px)', width: '100%'}} className='itemCentralizar'>
-                            
-                            {verificationItens()}
-
-                        </div>
-
-                        <button onClick={() => addItemHandler("lsdlsjdsjds", 10, imgIcon)}>add</button>
-                        <button onClick={() => removeItemHanler()}>remover</button>
-                    </div>
+                <button onClick={() => addItemHandler("lsdlsjdsjds", 10, imgIcon)}>add</button>
+                <button onClick={() => removeItemHanler()}>remover</button>
+            </div>
         </div>
     );
 }
