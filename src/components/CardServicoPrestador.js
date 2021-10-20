befa2a161a9268967a5bc8176ea69905ee8df371
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-20 09:46:22 -0300

    Estilizado card de serviço

--- src/components/CardServicoPrestador.js
+++ src/components/CardServicoPrestador.js
@@ -29,22 +29,25 @@ function CardServicoPrestador () {
         <div>
             {myServices.map((item, index)=>(
                 <div id={item.id} key={index} className="containerCard" style={{marginLeft: '10px', borderRadius: '2px', width: '300px', boxShadow: '0px 3px 9px rgba(0,0,0,0.2)', padding: '33px', marginBottom: '10px'}}>
-                    <div style={{display:"flex"}}>
+                    <div style={{display:"flex", alignItems: 'center', justifyContent: 'flex-start', marginBottom:'10px'}}>
                         <div>
-                            <img src={item.image} />
+                            <img src={item.image} style={{width:'22px', marginRight: '10px'}} />
                         </div>
                         <div className="titleCard">
-                            <h2>{item.title}</h2>
+                            <h2 style={{color: 'rgba(0,0,0,0.85)', fontSize: '16px',marginBottom:'0px'}}>{item.title}</h2>
                         </div>
                     </div>
                     <p>{item.prediction}</p>
                     {item.isFinished
-                     ?  <button className="btConcluir">Finalizar</button>
+                     ? <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <button className="btConcluir" style={{width:'91px', display: 'flex', alignItems:'center', justifyContent: 'center'}}>Finalizar</button>
+                        </div>
                      : <div style={{display: 'flex'}}>
                         <button className="btCancelar" style={{marginRight: '20px'}} onClick={()=>{ExcluirServico(item.id)}}>Excluir</button>
                         <button className="btProx" style={{background: 'green', border: 'none'}}>Aceitar</button>
                        </div>
                     }
+
                 </div>
             ))}
 
