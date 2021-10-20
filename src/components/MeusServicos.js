dbbe07e63a30cf2b32023a4e8648c23e83ef55e2
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-20 00:50:51 -0300

    Atualizado MeusServicos, para receber o novo card

--- src/components/MeusServicos.js
+++ src/components/MeusServicos.js
@@ -1,74 +1,25 @@
-
-
 import '../css/configCSS.css';
 import '../css/homePages.css';
 import '../css/button.css';
-import imgIcon from '../img/icones/advertencia.PNG';
-import imgIconFinalizado from '../img/icones/okay.PNG';
-
 import imgTask from '../img/icones/task.PNG';
-
-import Itens from './Itens';
-
-import React, {useState} from 'react';
+import React from 'react';
+import CardServicoPrestador from './CardServicoPrestador';
+import myServices from '../data/CardsPrestador';
 
 function MeusServicos(){
 
-    const [listaServico, setListaServico] = useState([]);
-    const [exemploId, setExemploId] = useState(0); //APAGAR ISSO DEPOIS
-
-    const addItemHandler = (titulo, dias, img) =>{
-        setExemploId(exemploId + 1);
-        let obj = {Id: exemploId, Titulo: titulo, Dias: dias, Img: img};
-        let arr = listaServico.concat(obj);
-        setListaServico(arr);
-    }
-
-    //CONTINUAR AQUIIIIIIIIIIIIIIIII
-    const removeItemHanler = () =>{
-
-        var list = listaServico;
-        list.splice(0, 1);
-
-        setListaServico(list);
-    }
-
-
-
-    
     let card;
     const verificationItens = () =>{
-         if (listaServico.length === 0){
+         if (myServices.length === 0){
             card=<>
                 <h5 style={{fontWeight: 'bold'}}>Nenhuma solicitação de serviços por aqui!</h5>
                 <h6>Estamos de olho por aqui, assim que uma nova solicitação chegar, você será avisado.</h6>
               </>
         }else{
-            card= <div style={{position: 'absolute',width: '80%', height: '520px', overflow: 'scroll', scrollbarWidth: 'none'}}>
-                    <h5 style={{color: '#1351B4', marginTop: '28px'}}>
-                        Solicitações
-                    </h5>
-
-                    <div style={{display: 'flex', marginTop: '2em'}}>
-                        {listaServico.map((obj, index) =>(
-                            <Itens key={index} typeCard="solicitacao" chave={obj.Id} previcao={obj.Dias} titulo={obj.Titulo} image={obj.Img}  />
-                        ))}
-                    </div>
-
-                    <h5 style={{color: '#135184', marginTop: '18px'}}>
-                        Em andamento
-                    </h5>
-
-                    <div style={{display: 'flex', marginTop: '2em'}}>
-                        {listaServico.map((obj,index) =>(
-                            <Itens key={index} typeCard="emAndamento" chave={obj.Id} previcao={obj.Dias} titulo={obj.Titulo} image={imgIconFinalizado}  />
-                        ))}
-                    </div>
-                </div>
+            card= <CardServicoPrestador/>
         }
-
     }
-    
+
     verificationItens()
 
     return (
@@ -96,13 +47,8 @@ function MeusServicos(){
                 </nav>
 
                 <div style={{height: 'calc(100vh - 156px)', width: '100%'}} className='itemCentralizar'>
-                    
                     {card}
-
                 </div>
-
-                <button onClick={() => addItemHandler("lsdlsjdsjds", 10, imgIcon)}>add</button>
-                <button onClick={() => removeItemHanler()}>remover</button>
             </div>
         </div>
     );
