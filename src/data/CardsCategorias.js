03f64b65d6ad2b621618a649a683f61e703ff82c
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-24 10:01:12 -0300

    Criando componente card e arquivos de categorias

---
+++ src/data/CardsCategorias.js
@@ -0,0 +1,49 @@
+const categorias = [
+    {
+        id: 0,
+        title: "Serviços Mecânicos",
+        image: notFinished
+    },
+    {
+        id: 1,
+        title: "Serviços Elétricos",
+        image: notFinished
+    },
+    {
+        id: 2,
+        title: "Serviços de Limpeza",
+        image: notFinished
+    },
+    {
+        id: 3,
+        title: "Pintura",
+        image: notFinished
+    },
+    {
+        id: 4,
+        title: "Frete",
+        image: notFinished
+    },
+    {
+        id: 5,
+        title: "Serviços Hidráulicos",
+        image: notFinished
+    },
+    {
+        id: 5,
+        title: "Serviços de Babá",
+        image: notFinished
+    },
+    {
+        id: 5,
+        title: "Serviços de Babá",
+        image: notFinished
+    },
+    {
+        id: 5,
+        title: "Ar-Condicionado",
+        image: notFinished
+    }    
+]
+
+export default myServices;
