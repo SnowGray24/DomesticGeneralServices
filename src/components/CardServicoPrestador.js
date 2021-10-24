5495737cd0152829777e6ae5f7dd6532323e66d9
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-24 01:47:48 -0300

    Corrigida função de exclusão do card, que agora pega o item correto

--- src/components/CardServicoPrestador.js
+++ src/components/CardServicoPrestador.js
@@ -1,49 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import myServices from "../data/CardsPrestador";
 import '../css/homePages.css';
+import { useRouteMatch } from "react-router";
 
 function CardServicoPrestador () {
 
-    const [services, setServices] = useState([...myServices]);
+    const [services, setServices] = useState(myServices);
+    const [tb, setTb] =useState([])
     const [finishedServices, setFinishedServices] = useState([]);
 
-    function ExcluirServico(idItem) {
+    
+    let newArray = []
 
-        let newArray = services
-        
-        newArray.splice(idItem, 1)
-        
+    newArray= services
+    
+    useEffect(()=>{
         setServices(newArray)
-        
-        let cardContainer = document.getElementsByClassName("containerCard")[idItem]
-        cardContainer.style.display="none"
+    },[])
+    
+
+    const ExcluirServico = (item)=> {
+
+        let index = newArray.indexOf(item)
+        console.log(index)
+        newArray.splice(index,1)
+
+        setServices(newArray)
+        let cardContainer = document.getElementsByClassName("containerCard")[index]
+        cardContainer.remove()
     }
+    
 
 
+    
     function FinalizarServico(idItem) {
         
         /* Retirando do Array de serviços em andamento*/
         let newArrayServices = services
-
-        var indexFinalizado = newArrayServices.indexOf(idItem)
         
-        let ServicoMarcadoComoFinalizado = newArrayServices.splice(indexFinalizado-1, 1)
+        let ServicoMarcadoComoFinalizado = newArrayServices.splice(idItem, 1)
+
         setServices(newArrayServices)
         
-      
         let cardContainer = document.getElementsByClassName("containerCard")[idItem]
-        cardContainer.style.display="none"
+        cardContainer.remove()
 
         /* Inserindo no Array de serviços finalizados*/
         let newArrayFinalizados = finishedServices
-
         newArrayFinalizados.push(...ServicoMarcadoComoFinalizado)
         setFinishedServices(newArrayFinalizados)
     }
     
     return (
         <div className="ContainerSolicitacoes">
-            {services.map((item, index)=>(
+            {
+            services.map((item, index)=>(
                 <div id={item.id} key={index} className="containerCard" style={{marginLeft: '10px', borderRadius: '2px', width: '300px', boxShadow: '0px 3px 9px rgba(0,0,0,0.2)', padding: '33px', marginBottom: '10px'}}>
                     <div style={{display:"flex", alignItems: 'center', justifyContent: 'flex-start', marginBottom:'10px'}}>
                         <div>
@@ -55,18 +66,18 @@ function CardServicoPrestador () {
                     </div>
                     <p style={{marginBottom: '25px', fontSize: '16px'}}>{item.prediction}</p>
                     {item.isFinished
-                     ? <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    ? <div style={{display: 'flex', justifyContent: 'flex-end'}}>
                         <button className="btFinalizarServicos" onClick={()=>{FinalizarServico(item.id)}}>Finalizar</button>
                         </div>
-                     : <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-                        <button className="btExcluirServicos" style={{marginRight: '20px'}} onClick={()=>{ExcluirServico(item.id)}}>Excluir</button>
+                    : <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <button className="btExcluirServicos" style={{marginRight: '20px'}} onClick={()=>{ExcluirServico(item)}}>Excluir</button>
                         <button className="btAceitarServicos">Aceitar</button>
-                       </div>
+                    </div>
                     }
 
                 </div>
-            ))}
-
+            ))
+            }
         </div>
     )
 }
