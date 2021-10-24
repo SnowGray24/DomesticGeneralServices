3b39752149da0d3738e1045cbfffd9f34c09fa02
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-24 10:07:43 -0300

    Criada estrutura básica do componente de card

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Card from '../data/CardsCategorias';
+
+function Card () {
+    return (
+        <div>
+            s
+        </div>
+    )
+}
+
+export default Card;
