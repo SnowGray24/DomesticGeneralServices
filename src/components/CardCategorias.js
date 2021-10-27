4f7f34a2c918a85c60fac1d8e16449a9bc02ac3d
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-27 11:38:16 -0300

    Inserindo Link nos cards de Categorias

--- src/components/CardCategorias.js
+++ src/components/CardCategorias.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardsCategorias from '../data/CardsCategorias';
 import '../css/categoriesCards.css';
+import { Link } from 'react-router-dom';
 
 function CardCategorias () {
     return (
@@ -8,10 +9,12 @@ function CardCategorias () {
             <h3>Encontre Serviços por Categoria</h3>
             <div className="CategoryCardsContainer">
                 {CardsCategorias.map((item, index)=>(
+                <Link className="Link-card" to="/">
                     <div key={index} className="categoryCard">
                         <img src={item.image} alt={item.title} />
                         <p className="titleCard">{item.title}</p>
                     </div>
+                </Link>
                 ))}
             </div>
         </div>
