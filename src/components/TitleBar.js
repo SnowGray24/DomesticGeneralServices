76001fd0974aed7648e233e43002f9a1ed3d55d4
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-27 10:39:45 -0300

    Criado arquivo de estilo para o TitleBar

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,8 +1,9 @@
 import imgTask from '../img/icones/task.PNG';
+import '../css/titleBar.css';
 
 function TitleBar({step1, step2, step3}) {
     return (
-        <div style={{marginLeft: '30px',height: '60px', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+        <div className="containerTitleBar">
             <div>
                 <img height="20" src={imgTask} alt="" /> {'>'} {step1} {step2} {step3}
             </div>
