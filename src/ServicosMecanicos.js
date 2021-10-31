b1591b77c7be6402713a8ad9c4972b82167f4b20
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-10-30 23:41:51 -0300

    Criada estrutura básica da main de Servicos Mecanicos

--- src/ServicosMecanicos.js
+++ src/ServicosMecanicos.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import MenuVertical from "./components/MenuVertical";
 import TitleBar from "./components/TitleBar";
 import OptionsMenuContratante from './data/OpcoesMenuContratante';
+import './css/servicosMecanicos.css';
 
 
 function ServicosMecanicos() {
@@ -17,7 +18,13 @@ function ServicosMecanicos() {
                 <section style={{width: '100%', color: 'rgba(0, 0, 0, 0.5)'}}>
                     <Header letraPerfil="J" nome="João"/>
                     <TitleBar step1="Todos os Serviços" route1="/Todos Serviços" step2="> Serviços Mecânicos" route2="/ServicosMecanicos"/>
-                    <CardPrestador/>
+                    <div className="main">
+                        <h3>Explore os Serviços Mecânicos</h3>
+                        <p>
+                            Problemas com sua máquina ou veículo? Podemos te ajudar!<br/>Você pode contratar um ecanico TORXS para qualquer serviço de manutenção de máquinas ou conserto de carros.<br/> Além disso Você pode encontrar um prestador de serviços por cidade ou serviço prestado.
+                        </p>
+                        <CardPrestador/>
+                    </div>
                 </section>
             </main>
         </div>
