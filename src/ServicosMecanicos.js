a77011a5895080e9e1cd0438703e6074ac29cdcd
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-11-01 10:41:58 -0300

    Criada seção de filtros

--- src/ServicosMecanicos.js
+++ src/ServicosMecanicos.js
@@ -5,6 +5,8 @@ import TitleBar from "./components/TitleBar";
 import OptionsMenuContratante from './data/OpcoesMenuContratante';
 import './css/layout.css';
 import SelectField from "./components/SelectField";
+import './css/servicosMecanicosScreen.css';
+import { Button } from "reactstrap";
 
 
 function ServicosMecanicos() {
@@ -13,6 +15,7 @@ function ServicosMecanicos() {
     options[0].selected = true;
 
     const selectCityOptions = ["Cidade","Pau dos Ferros", "Lucrécia", "São Miguel", "Alexandria"];
+    const selectServicesOptions = ["Nome do serviço","Mecânico geral", "Troca de óleo de motor", "Manutenção de embreagem", "Reparos simples", "Troca de peças"];
 
     return (
         <div className='page'>
@@ -26,7 +29,11 @@ function ServicosMecanicos() {
                         <p>
                             Problemas com sua máquina ou veículo? Podemos te ajudar!<br/>Você pode contratar um ecanico TORXS para qualquer serviço de manutenção de máquinas ou conserto de carros.<br/> Além disso Você pode encontrar um prestador de serviços por cidade ou serviço prestado.
                         </p>
-                        <SelectField options={selectCityOptions}/>
+                        <div className="filters">
+                            <SelectField options={selectCityOptions} width={89}/>
+                            <SelectField options={selectServicesOptions} width={149}/>
+                            <Button className="btProx">Buscar</Button>
+                        </div>
                         <CardPrestador/>
                     </div>
                 </section>
