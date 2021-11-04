3d01e48ece5084e845e66600505f93ac7626d6e7
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-11-03 23:00:12 -0300

    Adicionando link no card do serviço anunciado

--- src/components/CardPrestador.js
+++ src/components/CardPrestador.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ServicosMecanicos from "../data/AnunciosServicosMecanicos";
 import '../css/cardPrestador.css';
+import { Link } from 'react-router-dom';
 
 function CardPrestador() {
     return (
         <div className="CardsContainer">
             {ServicosMecanicos.map((item, index)=>(
-                <div key={index} className="CardContent">
+                <Link key={index} className="CardContent" to={item.route}>
                     <img src={item.imagem} alt={item.nome} className="PerfilPrestador"/>
                     <h3 className="NomePrestador">{item.nome}</h3>
                     <p className="DescPrestador">{item.descrição}</p>
-                </div>
+                </Link>
             ))}
         </div>
     )
